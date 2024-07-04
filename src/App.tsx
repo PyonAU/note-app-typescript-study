ba8539945c6979319a1b4b18f786b67f86903c7e
Pyon18Pyon <ayasayshi@gmail.com>
2024-07-04 12:01:52 +1000

    feat: added routing pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<h1>new</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
+
+// path="*" is to navigate users in case they type something that doesn't exist
